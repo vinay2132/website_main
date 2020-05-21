5f4865af10e51823dd1efb46dc77de0e29d7ef9a
vinay2132 <63947925+vinay2132@users.noreply.github.com>
2020-05-21 21:55:10 +0530

    Create forgotvalid.php

---
+++ forgotvalid.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+$con =mysqli_connect('localhost','root','');
+mysqli_select_db($con,'test2');
+$email = $_POST['email'];
+$s = "select * from signindetailss where email = '$email'";
+$result = mysqli_query($con,$s);
+$num = mysqli_num_rows($result);
+if($num == 1){
+    $userdata = mysqli_fetch_array($result);
+    $username = $userdata['username'];
+    $vkey = $userdata['vkey'];
+
+    $to = $email;
+          $subject ="email varification";
+          $message = "<h1>RoboKalam literature of robotics.</h1>";
+          $message .= "<h4>Hi $username, </h4><br>";
+          $message .= "<h4>Greetings from RoboKalam ! We welcome you aboard the RoboKalam community !</h4>";
+          $message .="<h4>Click the button below to set a new password . This is the email address on which your profile will be created</h4>";
+          $message .= "<a href='http://localhost/pp/website1.0/forgotvarified.php?vkey=$vkey'>new password</a>";
+          $headers ="from:vinayd.vef@gmail.com\r\n";
+          $headers .="MIME-Version:1.0"."\r\n";
+          $headers .="Content-type:text/html;charset=UTF-8"."\r\n";
+          mail($to,$subject,$message,$headers);
+          header('location:thanks.html');
+
+}
+else{
+    echo '<script>alert("Sorry this email donot exist")</script>';
+}
