d99c76d21a592596662972d3270f5b5f68d35107
Shubham Gujarathi <sgujarathi17699@users.noreply.github.com>
2020-05-22 09:52:17 +0530

    added login & about us & events

--- js/main.js
+++ js/main.js
@@ -67,3 +67,45 @@ $('.landing-slider').slick({
     autoplay: true,
     autoplaySpeed: 2000,
 });
+
+
+
+
+// =========================================
+// upcoing event slider
+// =========================================
+
+$('.event-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: ".slider-btn .prev",
+    nextArrow: ".slider-btn .next",
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
